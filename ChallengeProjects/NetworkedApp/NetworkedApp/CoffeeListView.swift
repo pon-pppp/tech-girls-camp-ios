f89935c5f60a08f9a61151e3d1d48c200c2fb264
pon-pppp <kutsushitaneko0305@gmail.com>
2025-08-31 15:24:39 +0900

    Update CoffeeListView.swift

--- ChallengeProjects/NetworkedApp/NetworkedApp/CoffeeListView.swift
+++ ChallengeProjects/NetworkedApp/NetworkedApp/CoffeeListView.swift
@@ -1,22 +1,39 @@
 import SwiftUI
 
 struct CoffeeListView: View {
-   
+    @State private var coffees: [Coffee] = []
+
     var body: some View {
-        ScrollView {
-            LazyVStack(spacing: 20) {
-                CoffeeItemView()
-                    .padding(.horizontal)
-                CoffeeItemView()
-                    .padding(.horizontal)
-                CoffeeItemView()
-                    .padding(.horizontal)
-                CoffeeItemView()
-                    .padding(.horizontal)
-                CoffeeItemView()
-                    .padding(.horizontal)
+        NavigationStack {
+            ScrollView {
+                LazyVStack(spacing: 20) {
+                    ForEach(coffees) { coffee in
+                        NavigationLink(destination: CoffeeDetailView(coffee: coffee)) {
+                                                    // ← アイテムをタップすると詳細画面へ遷移
+                                                    CoffeeItemView(coffee: coffee)
+                                                        .padding(.horizontal)
+                                                }
+                    }
+                    
+                }
+                .padding(.vertical)
+            }
+            .task {
+                do {
+                    coffees = try await getCoffees()
+                } catch {
+                    print("Failed to fetch coffees: \(error)")
+                }
             }
         }
+       
+    }
+
+    func getCoffees() async throws -> [Coffee] {
+            guard let url = URL(string: "https://api.sampleapis.com/coffee/hot") else { return [] }
+            let (data, _) = try await URLSession.shared.data(from: url)
+            let coffees = try JSONDecoder().decode([Coffee].self, from: data)
+            return coffees
     }
 }
 
