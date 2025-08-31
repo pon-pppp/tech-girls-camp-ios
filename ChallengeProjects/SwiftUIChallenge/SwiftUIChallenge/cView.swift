efee912274ae73193d02bcc5b2adcd1141522f96
pon-pppp <kutsushitaneko0305@gmail.com>
2025-08-31 12:00:35 +0900

    Update cView.swift

--- ChallengeProjects/SwiftUIChallenge/SwiftUIChallenge/cView.swift
+++ ChallengeProjects/SwiftUIChallenge/SwiftUIChallenge/cView.swift
@@ -9,7 +9,15 @@ import SwiftUI
 
 struct cView: View {
     var body: some View {
-        Text("C. テキストを四角形の左下に配置")
+        ZStack(alignment: .bottomLeading) {
+            Rectangle()
+                .fill(Color.green)
+                .frame(width: 200, height: 100)
+            Text("前面のテキスト")
+                .padding()
+                .foregroundColor(.white)
+        
+        }
     }
 }
 
